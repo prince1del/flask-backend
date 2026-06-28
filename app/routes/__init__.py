cb8eb1aed56939faff1c9d699e4cfe98c05283af
Prince Oberoi <prince@gmail.com>
2026-06-28 18:14:50 +0530

    Update route exports and related changes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,19 +3,19 @@ from .auth import auth_blueprint
 from .data import data_blueprint
 from .reports import reports_blueprint
 from .schemas import schemas_blueprint
-from .storage import bp as storage_bp
+from .storage import storage_bp
 from .target_achievement import target_achievement_bp
 from .workspaces import workspaces_blueprint
 from .party_matching import party_matching_bp
 
 __all__ = [
-    "analytics_blueprint",
-    "auth_blueprint",
-    "data_blueprint",
-    "reports_blueprint",
-    "schemas_blueprint",
-    "storage_bp",
-    "target_achievement_bp",
-    "workspaces_blueprint",
-    "party_matching_bp",
+    'target_achievement_bp',
+    'party_matching_bp',
+    'storage_bp',
+    'analytics_blueprint',
+    'auth_blueprint',
+    'data_blueprint',
+    'reports_blueprint',
+    'schemas_blueprint',
+    'workspaces_blueprint'
 ]
